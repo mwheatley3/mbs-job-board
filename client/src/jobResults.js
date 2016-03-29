dde0ef70db848308118d1f32b0904ad14857aa02
Michael Wheatley <michaelvwheatley@gmail.com>
2016-03-29 15:33:23 -0500

    code cleanup

--- client/src/jobResults.js
+++ client/src/jobResults.js
@@ -4,5 +4,5 @@ angular.module('myApp')
   .controller('JobResults', ['$scope','$http','Services', function($scope,$http,Services) {
   	$scope.results = Services.jobSearchResults.data || { jobs: [] };
   	$scope.jobs = $scope.results.jobs || [];
-  	$scope.indeedAttribution = '<span id=indeed_at><a href="https://www.indeed.com/">jobs</a> by <ahref="http://www.indeed.com/" title="Job Search"><img src="http://www.indeed.com/p/jobsearch.gif" style="border: 0;vertical-align: middle;" alt="Indeed job search"></a></span>'
+  	$scope.indeedAttribution = '<span id=indeed_at><a href="https://www.indeed.com/">jobs</a> by <ahref="http://www.indeed.com/" title="Job Search"><img src="https://www.indeed.com/p/jobsearch.gif" style="border: 0;vertical-align: middle;" alt="Indeed job search"></a></span>'
   }]);
